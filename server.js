3907a3bb2dcdc43490c6fd33a90d106a3e6acfb3
Kevin Son <kruny1001@gmail.com>
2015-07-04 21:49:19 -0500

    7/4

--- server.js
+++ server.js
@@ -36,24 +36,12 @@ require('./config/passport')();
 app.listen(config.port);
 
 
-app.use( function(req, res, next){
-		// Website you wish to allow to connect
-		res.setHeader('Access-Control-Allow-Origin', 'https://reviewbox.firebaseapp.com');
-
-		// Request methods you wish to allow
-		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-		// Request headers you wish to allow
-		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-		// Set to true if you need the website to include cookies in the requests sent
-		// to the API (e.g. in case you use sessions)
-		//res.setHeader('Access-Control-Allow-Credentials', true);
-
-		// Pass to next layer of middleware
-		next();
-	}
-);
+app.all('/*', function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header("Access-Control-Allow-Methods", "GET, POST","PUT");
+	next();
+});
 
 // Expose app
 exports = module.exports = app;
